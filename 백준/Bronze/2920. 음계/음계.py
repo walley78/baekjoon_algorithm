28b7f2a70b89334ce010745176cdc3670bc32980
walley78 <156279335+walley78@users.noreply.github.com>
2024-02-10 10:53:10 +0900

    [Bronze II] Title: 음계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+scale = list(map(int, input().split()))
+
+if sorted(scale) == scale:
+    print('ascending')
+elif sorted(scale, reverse=True) == scale:
+    print('descending')
+else:
+    print('mixed')
